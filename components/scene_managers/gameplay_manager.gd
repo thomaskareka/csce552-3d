fa12107bf47646340de6c640bb732a08396a2aa9
Thomas Kareka <tkareka@email.sc.edu>
2025-11-22 13:39:47 -0500

    player bounds

--- components/scene_managers/gameplay_manager.gd
+++ components/scene_managers/gameplay_manager.gd
@@ -10,7 +10,47 @@ func _init() -> void:
 	pass
 
 func _ready() -> void:
+	_init_bounds()
 	_start_game()
+
+func _init_bounds() -> void:
+	var bounds_max := player_bounds.position + player_bounds.size
+	var center := player_bounds.position + (player_bounds.size * 0.5)
+	var thickness := 1.0
+	
+	#left
+	_create_wall(
+		Vector3(player_bounds.position.x - thickness * 0.5, center.y, center.z),
+		Vector3(thickness, player_bounds.size.y + 2.0 * thickness, thickness)
+	)
+	#right
+	_create_wall(
+		Vector3(bounds_max.x + thickness * 0.5, center.y, center.z),
+		Vector3(thickness, player_bounds.size.y + 2.0 * thickness, thickness)
+	)
+	#top
+	_create_wall(
+		Vector3(center.x, bounds_max.y + thickness * 0.5, center.z),
+		Vector3(player_bounds.size.x + 2 * thickness, thickness, thickness)
+	)
+	#bottom
+	_create_wall(
+		Vector3(center.x, player_bounds.position.y - thickness * 0.5, center.z),
+		Vector3(player_bounds.size.x + 2 * thickness, thickness, thickness)
+	)
+
+func _create_wall(pos: Vector3, size: Vector3) -> void:
+	var wall := StaticBody3D.new()
+	add_child(wall)
+	wall.transform.origin = pos
+	
+	var cs = CollisionShape3D.new()
+	wall.add_child(cs)
+	
+	var shape := BoxShape3D.new()
+	shape.size = size
+	cs.shape = shape
+
 
 func _start_game() -> void:
 	for hs: HealthSystem in get_tree().get_nodes_in_group("health_system"):
